24982abc5e1a96ae963f82f935777300a8caea9e
Shariq Hirani <shariqhirani2010@gmail.com>
2021-09-14 11:35:48 -0500

    completed example

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 3000;
+const TEST_ENV = process.env.TEST_ENV || null;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(PORT);
+  console.log(TEST_ENV);
   await app.listen(PORT);
 }
+
 bootstrap();
-
-
-
-
-
-
